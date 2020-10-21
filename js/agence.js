a8dedb56221145bfa550a2cd83ae09ecf244fa24
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2020-10-22 09:13:02 +1100

    Structure & Bug fixes
    Modification de la structure du projet
    Modification du style du slider
    Fix d'un bug où le mauvais temps d'attente était affiché

--- js/agence.js
+++ js/agence.js
@@ -77,14 +77,8 @@ function Agence(pos, id, name, waitingTime, maxWaitingTime, sliderValue){
     }
 
     this.formatTime = function(){
-        var sec_num = parseInt(this.waitingTime, 10); // don't forget the second param
-        var hours   = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-        if (hours   < 10) {hours   = "0"+hours;}
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
-        return hours+'h'+minutes+'m'+seconds+'s';
+        return moment("2015-01-01").startOf('day')
+            .seconds(this.waitingTime/1000)
+            .format('HH:mm:ss');
     }
 }
