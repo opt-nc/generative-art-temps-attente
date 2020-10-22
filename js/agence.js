ca86fdfb1b4652e40e5a08c494bb1a77b843d739
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2020-10-22 15:23:12 +1100

    Fonctions mouseOver
    Ajout de zone de mouseOver
    Ajout du cercle de progression de la journée
    Ajout de la date
    Ajout de la légende

--- js/agence.js
+++ js/agence.js
@@ -6,21 +6,25 @@ function Agence(pos, id, name, waitingTime, maxWaitingTime, sliderValue){
     this.maxWaitingTime = maxWaitingTime;
 
     this.size = 25;
+    this.fixedSize = this.size;
+    this.dir = 0.2;
+
     this.angle = 0;
     this.speed = 0.01+random(0.01, 0.015);
     this.savedSpeed = this.speed;
     this.radius = pos*this.size;
+
     this.random = random(-10,-5);
+    this.color = map(this.waitingTime, 0, this.maxWaitingTime+10, 150, 255, true)+this.random
 
     this.history = [];
 
-    this.render = function(sliderValue){
+    this.renderDay = function(sliderValue){
         push();
         translate(width/2, height/2);
 
         noStroke();
-        let color = map(this.waitingTime, 0, this.maxWaitingTime+10, 150, 255)+this.random;
-        fill(color, 0, 255);
+        fill(this.color, 0, 255);
         let y = this.radius * sin(this.angle);
         let x = this.radius * cos(this.angle);
 
@@ -40,6 +44,30 @@ function Agence(pos, id, name, waitingTime, maxWaitingTime, sliderValue){
         pop();
     }
 
+    this.renderNight = function(sliderValue){
+        push();
+        translate(width/2, height/2);
+        noStroke();
+        fill(this.color, 0, 255);
+        let y = this.radius * sin(this.angle);
+        let x = this.radius * cos(this.angle);
+
+        this.showTrails(color, this.size, sliderValue);
+        this.history.splice(0,2);
+        this.mouseOver(x, y);
+
+        text(this.history,0,this.y+100);
+
+        circle(x, y, this.size);
+        this.size += this.dir;
+        if(this.size >= this.fixedSize){
+            this.dir = -0.2;
+        } else if(this.size <= 0){
+            this.dir = 0.2;
+        }
+        pop();
+    }
+
     this.updateDatas = function(newWaitingTime, newMaxWaitingTime) {
         this.waitingTime = newWaitingTime;
         this.maxWaitingTime = newMaxWaitingTime;
@@ -52,9 +80,9 @@ function Agence(pos, id, name, waitingTime, maxWaitingTime, sliderValue){
         }
 
         push();
+        fill(color, 0, 255, 100);
         for(let i = this.history.length-1; i >= 0; i--){
-            fill(color, 0, 255, 100);
-            circle(this.history[i][0], this.history[i][1], map(i, length, 0, size, 0));
+            circle(this.history[i][0], this.history[i][1], map(i, length, 0, size, 0, true));
         }
         pop();
     }
